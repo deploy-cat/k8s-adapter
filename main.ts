df86100e9f653611d9a2eb8fe8a4707172621160
adb-sh <git@adb.sh>
2023-01-06 18:47:45 +0100

    cleanup boilerplate

--- main.ts
+++ main.ts
@@ -21,14 +21,6 @@ app.use(async (ctx, next) => {
   ctx.response.headers.set("X-Response-Time", `${ ms }ms`);
 });
 
-
-const books = new Map<string, any>();
-books.set("1", {
-  id: "1",
-  title: "The Hound of the Baskervilles",
-  author: "Conan Doyle, Arthur",
-});
-
 const router = new Router();
 router
   .get("/", (ctx) => {
